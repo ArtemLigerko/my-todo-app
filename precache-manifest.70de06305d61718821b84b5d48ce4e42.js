542697e852ea4accbdd5f817ccf9c8d51a7fb9a8
Artem Ligerko <artmarket81@gmail.com>
2022-06-29 19:43:22 +0300

    Updates

---
+++ precache-manifest.70de06305d61718821b84b5d48ce4e42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "466108195203a3546015",
+    "url": "/my-todo-app/static/css/main.86968f15.chunk.css"
+  },
+  {
+    "revision": "466108195203a3546015",
+    "url": "/my-todo-app/static/js/main.46610819.chunk.js"
+  },
+  {
+    "revision": "0a79cbd09a92bf552a28",
+    "url": "/my-todo-app/static/js/1.0a79cbd0.chunk.js"
+  },
+  {
+    "revision": "1d94776efbcd5692b0ef",
+    "url": "/my-todo-app/static/js/runtime~main.1d94776e.js"
+  },
+  {
+    "revision": "9fad9f21e164646b4b1ba89f480e2abf",
+    "url": "/my-todo-app/index.html"
+  }
+];
