d17a82bb2f511c96eb2f29a3c626d2b9b14121b6
Artem Ligerko <artmarket81@gmail.com>
2022-07-17 14:39:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-todo-app/precache-manifest.efa7576bea403e577de9914729b6d066.js"
+  "/my-todo-app/precache-manifest.6a2ce7a62a4b5fe2d063591d6874af53.js"
 );
 
 workbox.clientsClaim();
